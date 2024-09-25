fa828ca2bf2c7a086b117f35af0dad0d93e385a7
Aaryan Shetty <44059614+AaryanHazCompter@users.noreply.github.com>
2024-09-24 18:11:17 -0600

    Update Shape class

--- app/src/main/java/com/example/abacus/Shape.java
+++ app/src/main/java/com/example/abacus/Shape.java
@@ -3,4 +3,25 @@ package com.example.abacus;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+
+    public Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
 }
