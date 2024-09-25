e94fd9ff126f77eeec0f9f3c98c5a6b0c6cb1e18
iacatmeow22 <ksli@ualberta.ca>
2024-09-24 18:49:33 -0600

    new change

--- app/src/main/java/com/example/abacus/Shape.java
+++ app/src/main/java/com/example/abacus/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
 
-    private String color = "yellow";
+    private String color = "blue";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
