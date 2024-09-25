2caca0ea0c61f83c61ee077f4e8c203568d8324b
Matthieu Larochelle <mrlaroch@ualberta.ca>
2024-09-24 18:12:51 -0600

    added triangle shape

---
+++ app/src/main/java/com/example/abacus/Triangle.java
@@ -0,0 +1,13 @@
+package com.example.abacus;
+
+public class Triangle extends Shape {
+    private Integer z;
+
+    public Integer getZ() {
+        return z;
+    }
+
+    public void setZ(Integer z) {
+        this.z = z;
+    }
+}
